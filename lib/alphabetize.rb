ec5d5335ba2cb094df16c92bf15f7eccc2809b19
kruchkamgar <kruchkamgar@gmail.com>
2018-07-16 21:57:37 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -8,10 +8,10 @@ def alphabetize(arr)
   arr.sort_by { |string| word_value = 0
     position = 0
     string.each_char { |letter|
-      word_value += ( ranking_base ** -position ).to_f * alphabet.index(letter)
+      string_value += ( ranking_base ** -position ).to_f * alphabet.index(letter)
       position += 1
     } 
-    word_value
+    string_value
   }
   # arr.sort_by { |string| esperanto_alphabet.index (string[0]) }
 end
